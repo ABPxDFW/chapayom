a6c29e7830d7bbe6c17f1f5c425866437ba00e19
Bobby P <abpupeerapitug@yahoo.com>
2017-02-20 11:39:53 -0600

    Created an index page and also added the images folder.

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: abpxninja
+ * Date: 2/20/2017
+ * Time: 11:28 AM
+ */
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Chapasyom</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="/bower_components/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/styles.css">
+
+</head>
+<body>
+
+
+
+<script src="/bower_components/jquery/dist/jquery.slim.min.js"></script>
+<script src="/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+</body>
+
+</html>
