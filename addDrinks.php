c493fb66ea469e675fbd914352c01af393f38668
Bobby P <abpupeerapitug@yahoo.com>
2017-03-13 21:30:47 -0500

    Modified the main pages by including new php files to remove code redundancy.

--- addDrinks.php
+++ addDrinks.php
@@ -6,56 +6,9 @@
  * Time: 12:24 PM
  */
 
+    require_once('header.php');
+    require_once('navbar.php');
 ?>
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <title>Chapasyom</title>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
-        <link rel="stylesheet" href="css/teststyle.css">
-
-    </head>
-    <body>
-
-        <nav class="navbar navbar-inverse" role="navigation">
-            <div class="container-fluid">
-                <!-- Brand and toggle get grouped for better mobile display -->
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="index.php"><img src="Images/ChaPayom-logo-green.png" class="img-responsive" width="200px"></a>
-                </div>
-
-                <!-- Collect the nav links, forms, and other content for toggling -->
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-
-
-                    <ul class="nav navbar-nav navbar-right">
-                        <li><a href="#">About</a></li>
-                        <li><a href="#">Menu</a></li>
-                        <li><a href="#">Gallery</a></li>
-                        <li><a href="#">Comments</a></li>
-                        <li><a href="#">Contact</a></li>
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Admin <span class="caret"></span></a>
-                            <ul class="dropdown-menu" role="menu">
-                                <li><a href="addIngred.php">Add Ingredients</a></li>
-                                <li><a href="#">Check Orders</a></li>
-                                <li><a href="addDrinks.php">Add New Drinks</a></li>
-                                <li class="divider"></li>
-                                <li><a href="#">Log Out</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div><!-- /.navbar-collapse -->
-            </div><!-- /.container-fluid -->
-        </nav>
 
         <div class="container-fluid">
 
@@ -92,12 +45,9 @@
 
         </div>
 
-
-
-
-
         <script src="bower_components/jquery/dist/jquery.slim.min.js"></script>
         <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-    </body>
 
-</html>
+<?php
+    require_once('footer.php');
+?>
--- index.php
+++ index.php
@@ -6,65 +6,15 @@
  * Time: 11:28 AM
  */
 
+    require_once('header.php');
+    require_once('navbar.php');
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>Chapasyom</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/teststyle.css">
-
-</head>
-    <body>
-
-        <nav class="navbar navbar-inverse" role="navigation">
-            <div class="container-fluid">
-                <!-- Brand and toggle get grouped for better mobile display -->
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="index.php"><img src="Images/ChaPayom-logo-green.png" class="img-responsive" width="200px"></a>
-                </div>
-
-                <!-- Collect the nav links, forms, and other content for toggling -->
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-
-
-                    <ul class="nav navbar-nav navbar-right">
-                        <li><a href="#">About</a></li>
-                        <li><a href="#">Menu</a></li>
-                        <li><a href="#">Gallery</a></li>
-                        <li><a href="#">Comments</a></li>
-                        <li><a href="#">Contact</a></li>
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Login <span class="caret"></span></a>
-                            <ul class="dropdown-menu" role="menu">
-                                <li><a href="addDrinks.php">Add Drink</a></li>
-                                <li><a href="addIngred.php">Add Ingredient</a></li>
-                                <li><a href="#">Something else here</a></li>
-                                <li class="divider"></li>
-                                <li><a href="#">Separated link</a></li>
-                                <li class="divider"></li>
-                                <li><a href="#">One more separated link</a></li>
-                            </ul>
-                        </li>
-
-                    </ul>
-                </div><!-- /.navbar-collapse -->
-            </div><!-- /.container-fluid -->
-        </nav>
 
 
 
         <script src="bower_components/jquery/dist/jquery.slim.min.js"></script>
         <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-    </body>
 
-</html>
+<?php
+    require_once('footer.php');
+?>
