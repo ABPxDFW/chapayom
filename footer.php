34ca3738c33abdf1b32d31e1e05d220fdb791817
Bobby P <abpupeerapitug@yahoo.com>
2017-03-13 21:31:50 -0500

    Created and added the new files.

---
+++ footer.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: abpxninja
+ * Date: 3/13/2017
+ * Time: 9:15 PM
+ */
+?>
+
+        <p>Copyright</p>
+
+    </body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: abpxninja
+ * Date: 3/13/2017
+ * Time: 9:24 PM
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Chapasyom</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/teststyle.css">
+
+</head>
