44dfedb65053fd917d82be1b2b39c53724d30fb1
Bobby P <abpupeerapitug@yahoo.com>
2017-03-17 23:41:15 -0500

    Created new files.

---
+++ comment.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: abpxninja
+ * Date: 3/17/2017
+ * Time: 11:40 PM
+ */
+require_once('startsession.php');
+require_once('header.php');
+require_once ('connectVars.php');
+require_once('navbar.php');
+?>
+    <div class="container white">
+        <h3>Comment</h3>
+
+    </div>
+<?php
+require_once('footer.php');
+?>
---
+++ menu.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: abpxninja
+ * Date: 3/17/2017
+ * Time: 11:38 PM
+ */
+
+require_once('startsession.php');
+require_once('header.php');
+require_once ('connectVars.php');
+require_once('navbar.php');
+?>
+    <div class="container white">
+        <h3>Menu</h3>
+
+    </div>
+<?php
+require_once('footer.php');
+?>
