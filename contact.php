c56a5ed62ed6e1354f9911f3e4f4f80b9bb71db8
Bobby P <abpupeerapitug@yahoo.com>
2017-04-02 18:05:49 -0500

    Added the contact page.

--- contact.php
+++ contact.php
@@ -54,6 +54,45 @@ require_once('navbar.php');
             </div>
         </div>
 
+
+    <!-- Add Google Maps-->
+
+    <script src="https://maps.googleapis.com/maps/api/js?key="></script>
+    <script>
+        <!-- Google Map Location -->
+        var myCenter = new google.maps.LatLng(13.825494, 100.4713717);
+
+        function initialize() {
+            var mapProp = {
+                center: myCenter,
+                zoom: 16,
+                scrollwheel: false,
+                draggable: false,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+
+            var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+            var marker = new google.maps.Marker({
+                position: myCenter,
+            });
+
+            marker.setMap(map);
+        }
+
+        google.maps.event.addDomListener(window, 'load', initialize);
+
+        // Modal Image Gallery
+        function onClick(element) {
+            document.getElementById("img01").src = element.src;
+            document.getElementById("modal01").style.display = "block";
+            var captionText = document.getElementById("caption");
+            captionText.innerHTML = element.alt;
+        }
+
+
+    </script>
+
 <?php
     require_once('footer.php');
 ?>
